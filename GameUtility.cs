53af7547c29b83da15c94facf10d5a0df2eb0640
Luke A Chase <chase.luke.a@gmail.com>
2016-03-31 23:44:42 -0500

    Add GameUtility

---
+++ GameUtility.cs
@@ -0,0 +1,56 @@
+﻿/**
+ * GameUtility Script
+ * by Luke A Chase - chase.luke.a@gmail.com
+ * 
+ * -------------------------------------------------------------
+ * -------------------------------------------------------------
+*/
+
+using UnityEngine;
+using System;
+
+
+public static class GameUtility
+{
+
+	// returns unsigned value of a signed int
+	public static int unsignedValue(int number)
+	{
+		return (int)Mathf.Sqrt (Mathf.Pow (number, 2));
+	}
+
+	// returns unsigned value of a signed float
+	public static float unsignedValue(float number)
+	{
+		return Mathf.Sqrt (Mathf.Pow (number, 2));
+	}
+}
+
+
+public enum Axis
+{
+	x,
+	y,
+	z
+}
+
+
+public enum Direction
+{
+	Positive = 1,
+	Negative = -1
+}
+
+
+public enum MoveTo
+{
+	Origin,
+	Destination
+}
+
+
+public enum Layer
+{
+	Everything = 1,
+	Player = 8
+}
